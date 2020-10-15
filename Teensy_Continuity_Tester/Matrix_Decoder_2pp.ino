cccaa471f923836de231d7c2ccb383ae24368c39
Frank Adams <thedalles77@gmail.com>
2020-10-15 15:37:43 -0700

    Update Matrix_Decoder_2pp.ino

--- Teensy_Continuity_Tester/Matrix_Decoder_2pp.ino
+++ Teensy_Continuity_Tester/Matrix_Decoder_2pp.ino
@@ -46,7 +46,7 @@
 // and pin 36 are both grounds. They will be reported as tied together but they are not needed by the key matrix. In this case, set the 
 // min_pin to 2 or the max_pin to 35.
 //  
-int max_pin = 36; // the keyboard FPC connector pin count. If set to 34, unsolder the LED or the code won't work
+int max_pin = 36; // the keyboard FPC connector pin count. 
 int min_pin = 1; // the first pin to be tested on the FPC connector (usually pin 1)
 //
 // load the key codes used in sending usb numbers, tab, and down arrow
