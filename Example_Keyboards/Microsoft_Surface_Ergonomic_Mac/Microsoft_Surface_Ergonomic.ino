8f158770c405a78139f575990c4cb1ef0cfd00f8
Stephen Sweetland <stephen@projectlambda.com>
2021-05-10 22:31:14 +0100

    ... and pull needless import

--- Example_Keyboards/Microsoft_Surface_Ergonomic_Mac/Microsoft_Surface_Ergonomic.ino
+++ Example_Keyboards/Microsoft_Surface_Ergonomic_Mac/Microsoft_Surface_Ergonomic.ino
@@ -1,5 +1,3 @@
-#include <Keyboard.h>
-
 /* Copyright 2020 Frank Adams
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
