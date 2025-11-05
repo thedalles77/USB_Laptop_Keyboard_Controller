01ff3e719d5d2da8ed563a277e0859b6d549ef2b
Frank Adams <33405607+thedalles77@users.noreply.github.com>
2025-11-05 11:18:54 -0800

    Update code_Dell1545.py

--- Example_Keyboards/Pico_26Pin_Keyboard/code_Dell1545.py
+++ Example_Keyboards/Pico_26Pin_Keyboard/code_Dell1545.py
@@ -1,4 +1,19 @@
-# GPIO connections and key matrix for a Dell 1545 laptop keyboard
+#   Copyright 2025 Frank Adams
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#       http://www.apache.org/licenses/LICENSE-2.0
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#   The following GPIO connections and key matrix are for a Dell 1545 laptop keyboard.
+#   Use this KMK code as your starting point if using a different keyboard.
+#   Change the name to code.py when copying this file to the Pi Pico.
+#   See step 16 of my Instructable for more information.
+#
 import board
 
 from kmk.kmk_keyboard import KMKKeyboard
