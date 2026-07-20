99108060abcc17b3fa4e7ebc02e2749292ce2f34
Frank Adams <33405607+thedalles77@users.noreply.github.com>
2026-07-20 01:07:18 -0700

    Add files via upload

---
+++ Example_Keyboards/Pico_T61_Keyboard/QMK_USB/keymap.c
@@ -0,0 +1,44 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+//
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+//
+    [0] = LAYOUT(
+        KC_GRV,   KC_1,     KC_Q,     KC_TAB,   KC_A,     KC_ESC,   KC_Z,     KC_NO,
+		KC_F1,    KC_2,     KC_W,     KC_CAPS,  KC_S,     KC_NO,    KC_X,     KC_NO,
+		KC_F2,    KC_3,     KC_E,     KC_F3,    KC_D,     KC_F4,    KC_C,     KC_NO,
+		KC_5,     KC_4,     KC_R,     KC_T,     KC_F,     KC_G,     KC_V,     KC_B,
+		KC_6,     KC_7,     KC_U,     KC_Y,     KC_J,     KC_H,     KC_M,     KC_N,
+		KC_EQL,   KC_8,     KC_I,     KC_RBRC,  KC_K,     KC_F6,    KC_COMM,  KC_NO,
+		KC_F8,    KC_9,     KC_O,     KC_F7,    KC_L,     KC_NO,    KC_DOT,   KC_NO,
+		KC_MINS,  KC_0,     KC_P,     KC_LBRC,  KC_SCLN,  KC_QUOT,  KC_NO,    KC_SLSH,
+		KC_F9,    KC_F10,   KC_NO,    KC_BSPC,  KC_BSLS,  KC_F5,    KC_ENT,   KC_SPC,
+		KC_INS,   KC_F12,   KC_NO,    KC_NO,    MO(1),    KC_NO,    KC_NO,    KC_RGHT,
+		KC_DEL,   KC_F11,   KC_VOLU,  KC_VOLD,  KC_MUTE,  KC_NO,    KC_NO,    KC_DOWN,
+		KC_PGUP,  KC_PGDN,  KC_LGUI,  KC_NO,    KC_APP,   KC_NO,    KC_NO,    KC_NO,
+		KC_HOME,  KC_END,   KC_NO,    KC_NO,    KC_NO,    KC_UP,    KC_NO,    KC_LEFT,
+		KC_NO,    KC_PSCR,  KC_NO,    KC_NO,    KC_NO,    KC_LALT,  KC_NO,    KC_RALT,
+		KC_NO,    KC_NO,    KC_NO,    KC_LSFT,  KC_NO,    KC_NO,    KC_RSFT,  KC_NO,
+		KC_LCTL,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_RCTL,  KC_NO
+    ),
+    [1] = LAYOUT(
+        KC_GRV,   KC_1,     KC_Q,     KC_TAB,   KC_A,     KC_ESC,   KC_Z,     KC_NO,
+		KC_F1,    KC_2,     KC_W,     KC_CAPS,  KC_S,     KC_NO,    KC_X,     KC_NO,
+		KC_F2,    KC_3,     KC_E,     KC_F3,    KC_D,     KC_F4,    KC_C,     KC_NO,
+		KC_5,     KC_4,     KC_R,     KC_T,     KC_F,     KC_G,     KC_V,     KC_B,
+		KC_6,     KC_7,     KC_U,     KC_Y,     KC_J,     KC_H,     KC_M,     KC_N,
+		KC_EQL,   KC_8,     KC_I,     KC_RBRC,  KC_K,     KC_F6,    KC_COMM,  KC_NO,
+		KC_F8,    KC_9,     KC_O,     KC_F7,    KC_L,     KC_NO,    KC_DOT,   KC_NO,
+		KC_MINS,  KC_0,     KC_P,     KC_LBRC,  KC_SCLN,  KC_QUOT,  KC_NO,    KC_SLSH,
+		KC_EJCT,  KC_F10,   KC_NO,    KC_BSPC,  KC_BSLS,  KC_F5,    KC_ENT,   KC_SPC,
+		KC_INS,   KC_F12,   KC_NO,    KC_NO,    KC_TRNS,  KC_NO,    KC_NO,    KC_RGHT,
+		KC_DEL,   KC_F11,   KC_VOLU,  KC_VOLD,  KC_MUTE,  KC_NO,    KC_NO,    KC_DOWN,
+		KC_PGUP,  KC_PGDN,  KC_LGUI,  KC_NO,    KC_APP,   KC_NO,    KC_NO,    KC_NO,
+		KC_BRIU,  KC_BRID,  KC_NO,    KC_NO,    KC_NO,    KC_MSTP,  KC_NO,    KC_MPRV,
+		KC_NO,    KC_PSCR,  KC_NO,    KC_NO,    KC_NO,    KC_LALT,  KC_NO,    KC_RALT,
+		KC_NO,    KC_NO,    KC_NO,    KC_LSFT,  KC_NO,    KC_NO,    KC_RSFT,  KC_NO,
+		KC_LCTL,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_RCTL,  KC_NO
+    )
+};
