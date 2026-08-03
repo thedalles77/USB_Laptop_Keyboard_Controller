b7594b1213e7d9c4c3025a145aa5647426ac7383
Frank Adams <33405607+thedalles77@users.noreply.github.com>
2026-08-03 11:40:52 -0700

    Restore copyright and SPDX license headers

--- Example_Keyboards/dell_1545_qmk/dell1545_teensypp2p0/keymaps/default/keymap.c
+++ Example_Keyboards/dell_1545_qmk/dell1545_teensypp2p0/keymaps/default/keymap.c
@@ -2,9 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//     
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {     
     [0] = LAYOUT(
         KC_NO,   KC_INS,  KC_NO,   KC_F12,  KC_NO,   KC_NO,   KC_NO,   KC_RGHT,
 		KC_NO,   KC_DEL,  KC_NO,   KC_F11,  KC_NO,   KC_NO,   KC_NO,   KC_DOWN,
