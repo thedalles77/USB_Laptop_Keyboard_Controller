cfe3b84563d909612701751129baa162e01be857
Frank Adams <33405607+thedalles77@users.noreply.github.com>
2026-08-04 14:15:20 -0700

    Add files via upload

--- Example_Keyboards/Pico_26Pin_Keyboard/Pico_W_Dell1545_BLE/Pico_W_Dell1545_BLE.ino
+++ Example_Keyboards/Pico_26Pin_Keyboard/Pico_W_Dell1545_BLE/Pico_W_Dell1545_BLE.ino
@@ -48,10 +48,10 @@ bool lastKeyState[NUM_ROWS][NUM_COLS] = {false}; // this creates a 2D array fill
 
 void setup() {
   for (int r = 0; r < NUM_ROWS; r++) {
-    pinMode(rowPins[r], INPUT_PULLUP); // Make the row gpio's inputs with pullups
+    pinMode(rowPins[r], INPUT); // Make the row gpio's float by making them inputs w/o pullups to start.
   }
   for (int c = 0; c < NUM_COLS; c++) {
-    pinMode(colPins[c], INPUT); // Make the column gpio's float by making them inputs w/o pullups to start.
+    pinMode(colPins[c], INPUT_PULLUP); // Make the column gpio's inputs with pullups
   }
 
   KeyboardBLE.begin("Dell1545 Keyboard", "Dell Mod"); // This defines the Device Name that
@@ -62,14 +62,14 @@ void setup() {
 }
 
 void loop() {
-  // Single Pass Matrix Scan drives each column low, one at a time and reads all the rows
-  for (int c = 0; c < NUM_COLS; c++) {
-    pinMode(colPins[c], OUTPUT); // make the selected column pin an output (it was floating as an input)
-    digitalWrite(colPins[c], LOW); // drive the column pin low 
-    delayMicroseconds(10); // give it some time to settle out
+  // Single Pass Matrix Scan drives each row low, one at a time and reads all the columns with pull ups inside RP2040
+  for (int r = 0; r < NUM_ROWS; r++) {
+    pinMode(rowPins[r], OUTPUT); // make the selected row pin an output (it was floating as an input)
+    digitalWrite(rowPins[r], LOW); // drive the row pin low 
+    delayMicroseconds(20); // 10us sometimes causes multiple keys b/c slow rise time from 50K ohm pullups in RP2040
 
-    for (int r = 0; r < NUM_ROWS; r++) { // read each row, looking for a low that indicates the key is pushed
-      bool currentPressed = (digitalRead(rowPins[r]) == LOW);
+    for (int c = 0; c < NUM_COLS; c++) { // read each column, looking for a low that indicates the key is pushed
+      bool currentPressed = (digitalRead(colPins[c]) == LOW);
 
       if (currentPressed != lastKeyState[r][c]) { // check if current key state is not the same as the last loop
         uint8_t standardKey = keyMap[r][c]; // save the name of the newly pressed (or released) key from the matrix
@@ -85,7 +85,7 @@ void loop() {
         lastKeyState[r][c] = currentPressed; // save state of the key to the last key state array for next loop
       }
     }
-    pinMode(colPins[c], INPUT); // return column that was driven low to an input so it floats 
+    pinMode(rowPins[r], INPUT); // return row that was driven low to an input so it floats 
   }
   delay(8); // overall 8 msec keyboard scan rate is slow enough to eliminate any chance of key bounce
 }
